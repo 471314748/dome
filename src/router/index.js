d870f1b41df2fa7f4932a4b5605a8116d5236f91
GuoRong <471314748@qq.com>
2020-09-24 07:07:35 +0800

    ECharts基本使用

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dome from '../views/dome/dome.vue'
+import Echarts from '../views/ECharts/echarts.vue'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,10 @@ const routes = [
   {
     path: '/',
     component: Dome
+  },
+  {
+    path: '/echarts',
+    component: Echarts
   }
 ]
 
